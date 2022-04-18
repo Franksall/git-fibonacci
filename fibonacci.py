01fa730a04169f878072e550bd8039dc6806d22e
Frank Duarte <frnk.duor@gmail.com>
2022-04-17 23:58:19 -0500

    Escribimos comentarios en el codigo, para una mayor comprension

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,7 @@
 def fibonacci_iterativo(posicion, debe_imprimir):
     actual = 0
     siguiente = 1
-    for x in range(posicion + 1):
+    for x in range(posicion + 1): #un for para que haci en el rango de la posiccion dada imprima su fibonacci
         if debe_imprimir:
             print(str(actual) + ",", end="")
         temporal = actual
@@ -14,12 +14,12 @@ def fibonacci_recursivo(posicion):
         return posicion
     return fibonacci_recursivo(posicion - 1) + fibonacci_recursivo(posicion - 2)
 posicion = int(input("Fibonacci de: "))
-
+# Imprime los fibonacci de los numeros hasta la posicion dada
 print(f"Imprimiendo serie de numeros hasta la  posición {posicion}")
 fibonacci_iterativo(posicion, True)
-
+# Obtiene el valor pero no lo imprime con método iterativo
 valor = fibonacci_iterativo(posicion, False)
 print(f"\nFibonacci de {posicion} con metodo iterativo es {valor}")
-
+# Obtiene el valor pero no lo imprime con método recursivo
 valor = fibonacci_recursivo(posicion)
 print(f"Fibonacci de {posicion} con metodo recursivo es {valor}")
