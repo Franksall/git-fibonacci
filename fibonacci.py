b0ccbdb536cbf992b140de7a0c19ebb1694bb11c
Frank Duarte <frnk.duor@gmail.com>
2022-04-17 23:54:01 -0500

    Creando la salida del fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -13,3 +13,13 @@ def fibonacci_recursivo(posicion):
     if posicion < 2:
         return posicion
     return fibonacci_recursivo(posicion - 1) + fibonacci_recursivo(posicion - 2)
+posicion = int(input("Fibonacci de: "))
+
+print(f"Imprimiendo serie de numeros hasta la  posición {posicion}")
+fibonacci_iterativo(posicion, True)
+
+valor = fibonacci_iterativo(posicion, False)
+print(f"\nFibonacci de {posicion} con método iterativo es {valor}")
+
+valor = fibonacci_recursivo(posicion)
+print(f"Fibonacci de {posicion} con método recursivo es {valor}")
