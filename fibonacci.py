0c3477d44078c222d982110bf1066f3dc59d0fe3
Frank Duarte <frnk.duor@gmail.com>
2022-04-17 23:51:52 -0500

    Terminando de crear los dos metodos

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,14 @@
 def fibonacci_iterativo(posicion, debe_imprimir):
+    actual = 0
+    siguiente = 1
+    for x in range(posicion + 1):
+        if debe_imprimir:
+            print(str(actual) + ",", end="")
+        temporal = actual
+        actual = siguiente
+        siguiente = siguiente + temporal
+    return temporal
 
-    
 def fibonacci_recursivo(posicion):
     if posicion < 2:
         return posicion
