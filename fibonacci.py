9acd2d95870a5c7ca84bd15ad049433fdc7fc35a
Frank Duarte <frnk.duor@gmail.com>
2022-04-23 01:18:19 -0500

    Se terminode crear, se probo y despues de varios errores se llego auna solcion

--- fibonacci.py
+++ fibonacci.py
@@ -1,18 +1,18 @@
 #Creamos el menu iterativo para que si nos podamos el fibonacci que 
 #quedramos hallar
 menu = """
-
+Menu
 1.-Fibonacci manera iterativa
 2.-Fibonacci manera recursiva
 3.-Fibonacci manera Memoria
 """
 print(menu)
 op=int(input("Elige alguna opcion: "))
-if op is 1:#Se añade el fibonaci iterativo para en al priemra opcion para poder elegirlo
+if op is 1:
     def fibonacci_iterativo(posicion, debe_imprimir):
         actual = 0
         siguiente = 1
-        for x in range(posicion + 1):#un for para que asi en el rango de la posiccion dada imprima su fibonacci
+        for x in range(posicion + 1):#un for para que haci en el rango de la posiccion dada imprima su fibonacci
             if debe_imprimir:
                 print("[",x,"] = ",str(actual,) + "\n", end="")
             temporal = actual
@@ -41,7 +41,7 @@ elif op is 2:
     # Obtiene el valor pero no lo imprime con método recursivo
     print(f"Fibonacci de {posicion} con método recursivo es ",fibonacci_recursivo(posicion))
 elif op is 3:
-	#añadimos la ultima opcion de que seria un fibonacci de metodo memoria
+    #añadimos la ultima opcion de que seria un fibonacci de metodo memoria
     nums = {}
     def fibmemoria (n) :
         
@@ -53,9 +53,13 @@ elif op is 3:
             num = fibmemoria(n-1) + fibmemoria (n-2)
             nums [n]=num
             return num
+    #Mejoramos el codigo del fibonacci en memoria
     posicion = int(input("Fibonacci de: "))
     for n1 in range(1,posicion):
         print("[",n1,"] = ",fibmemoria(n1))
     print("Fibonaci de ",posicion," en metodo memoria es: ",fibmemoria(posicion))
 else:
     print("Tecla error")
+
+
+
