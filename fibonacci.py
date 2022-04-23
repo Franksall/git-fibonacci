c60a4a805578d9ff03b171956af16c709a895571
Frank Duarte <frnk.duor@gmail.com>
2022-04-23 00:53:20 -0500

    Creamo el switch en python

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,22 @@
 #Creamos el menu iterativo para que si nos podamos el fibonacci que 
 #quedramos hallar
-
 menu = """
 Menu
 1.-Fibonacci manera iterativa
 2.-Fibonacci manera recursiva
 3.-Fibonacci manera Memoria
 """
+print(menu)
+op=int(input("Elige alguna opcion: "))#Creamos el swicht en python que seria con ifs, 
+#para que asi podamos elgir la opcion
+if op is 1:
+   print("Tecla error")
+elif op is 2:
+    print("Tecla error")
+elif op is 3:
+    print("Tecla error")
+else:
+    print("Tecla error")
+
+
+
