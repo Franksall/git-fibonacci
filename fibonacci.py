21ca4b6ef3dd16bb1ffafb5c2c58b9e033c034a7
Frank Duarte <frnk.duor@gmail.com>
2022-05-16 08:53:02 -0500

    Agregamos el fib recursivo

--- fibonacci.py
+++ fibonacci.py
@@ -13,3 +13,18 @@ for v in xite:                     #Secuencialmente valores de la función de la
     fn.append(fibonacci(v))
 
 print("fib Iterativo (n)=",fn)
+def fibonaccir(n):
+    if n == 0:
+        return 0
+    if n <= 2:
+        return 1
+    else:
+      v=fibonaccir(n-1) + fibonaccir(n-2)
+    return v
+
+xr = list(range(1,11))           # 1..10 lista de valores
+fr = []             # A su vez es el cuadrado de n, el cubo de n, el valor de n Fibonacci
+for v in xr:                     #Secuencialmente valores de la función de la computadora
+    fr.append(fibonaccir(v))
+
+print("fib Recursivo(n)=",fr)
